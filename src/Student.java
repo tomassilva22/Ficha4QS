fe527d0198f768407bee8571ff2d2f5d144c0bdc
Diogo <diogoluisteixeira@gmail.com>
2023-10-06 10:50:14 +0100

    Full Java Doc

--- src/Student.java
+++ src/Student.java
@@ -1,69 +1,80 @@
+/**
+ * This class represents a student who parks at the UPT parking lot.
+ */
 public class Student {
 
-    private String name;
-    private int number;
-    private String licensePlate;
-    private int totalTime;
+    private String name; // Student's name
+    private int number; // Student's number
+    private String licensePlate; // Student's car license plate
+    private int totalTime; // Total time parked in minutes
 
     /**
+     * Get the student's name.
      *
-     * @return student name
+     * @return The student's name.
      */
     public String getName() {
         return name;
     }
 
     /**
+     * Set the student's name.
      *
-     * @param name
+     * @param name The student's name.
      */
     public void setName(String name) {
         this.name = name;
     }
 
     /**
+     * Get the student's number.
      *
-     * @return student number
+     * @return The student's number.
      */
     public int getNumber() {
         return number;
     }
 
     /**
+     * Set the student's number.
      *
-     * @param number
+     * @param number The student's number.
      */
     public void setNumber(int number) {
         this.number = number;
     }
 
     /**
+     * Get the student's car license plate.
      *
-     * @return student license plate
+     * @return The student's car license plate.
      */
     public String getLicensePlate() {
         return licensePlate;
     }
 
     /**
+     * Set the student's car license plate.
      *
-     * @param licensePlate
+     * @param licensePlate The student's car license plate.
      */
     public void setLicensePlate(String licensePlate) {
         this.licensePlate = licensePlate;
     }
 
     /**
+     * Get the total time parked by the student in minutes.
      *
-     * @return student total time parked
+     * @return The total time parked in minutes.
      */
     public int getTotalTime() {
         return totalTime;
     }
 
     /**
+     * Set the total time parked by the student in minutes.
      *
-     * @param totalTime
+     * @param totalTime The total time parked in minutes.
      */
     public void setTotalTime(int totalTime) {
         this.totalTime = totalTime;
